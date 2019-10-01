8d86c8a51c9e6aeb644324f2899d5bf2e8cf8b16
Hendrik Knoche <hendrik.knoche@gmail.com>
2019-10-01 18:53:51 +0200

    building aggregates

--- dataimport.R
+++ dataimport.R
@@ -1,5 +1,6 @@
 #import data from xlsx
 library(here)
+library(dplyr)
 
 dx<-NULL
 df<-NULL
@@ -10,4 +11,20 @@ for(i in 1:24){
   }
 
 df<-df[!df$Speed==2,]
-df[df$Correct.drone==TRUE,]$outcome<-'TP'
+# df[df$Correct.drone==TRUE,]$outcome<-'TP'
+df$outcome<-ifelse(is.na(df$Correct.drone),"FN", ifelse(df$Correct.drone==FALSE,"FP","TP"))
+ppm<-sqldf::sqldf('select participant as PID, speed, outcome, count(participant) as NumsOf from df group by participant, speed,outcome')
+
+ppmx<-sqldf::sqldf('select participant, speed, count(participant) as NumsOf from df group by participant, speed')
+df<-merge(df,ppmx)
+
+ppmy<-sqldf::sqldf('select participant, speed, outcome, count(participant) as NumsOf from df group by participant, speed, outcome')
+
+
+dfsummary<-sqldf::sqldf('select participant, speed, count(participant) as NumsOf from df  where participant>-1 group by participant, speed')
+
+df$precision<-df$
+# df %>%
+#   group_by(Participant,Speed) %>%
+#   mutate(Count=n())
+
